d91f532caf2e11776f5c9b66fc7b2720b104990d
ilyasakbergen <ilijas9813@gmail.com>
2020-05-28 08:37:53 +0600

    moved component scripts to section

--- app/Http/Controllers/Web/OrganizationController.php
+++ app/Http/Controllers/Web/OrganizationController.php
@@ -27,70 +27,34 @@ class OrganizationController extends Controller
             ? $this->organizationService->searchPaginatedOrganizations($search_key, $perPage)
             : $this->organizationService->getAllPaginatedOrganizations($perPage);
 
-        return view('web.organizations', compact('organizations'));
+        return view('web.organizations.index', compact('organizations'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        return view('web.organizations.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         //
---
+++ resources/views/web/organizations/create.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container-fluid">
+        <!-- Page Heading -->
+        {{--  todo org form      --}}
+        <h1 class="h3 mb-2 text-gray-800">Новая организация</h1>
+    </div>
+@endsection
